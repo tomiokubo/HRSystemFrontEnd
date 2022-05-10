de069e84e6ead2fd5eb93ee8db78236447043f0a
Tomio Kuboyama <viniciuskuboyama@gmail.com>
2022-05-10 00:16:35 -0300

    Login system implemented

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,17 +1,24 @@
 import * as types from '../types';
 
-const initalState = {
+const initialState = {
   isLoggedIn: false,
   token: false,
   user: {},
   isLoading: false,
 };
 
-export default function (state = initalState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
-    case types.LOGIN_REQUEST: {
+    case types.LOGIN_SUCCESS: {
       const newState = { ...state };
-      newState.clickedButton = !newState.clickedButton;
+      newState.isLoggedIn = true;
+      newState.token = action.payload.token;
+      newState.user = action.payload.user;
+      return newState;
+    }
+
+    case types.LOGIN_FAILURE: {
+      const newState = { ...initialState };
       return newState;
     }
 
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,7 +1,35 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+
 import * as actions from './actions';
 import * as types from '../types';
+import axios from '../../../services/axios';
+import history from '../../../services/history';
 
-function* loginRequest({ payload }) {}
+function* loginRequest({ payload }) {
+  try {
+    const response = yield call(axios.post, '/tokens', payload);
+    yield put(actions.loginSuccess({ ...response.data }));
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+    toast.success('Successful login');
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+    history.push(payload.prevPath);
+  } catch (error) {
+    toast.error('Invalid user or password');
+    yield put(actions.loginFailure());
+  }
+}
+
+function persistRehydrate({ payload }) {
+  const token = get(payload, 'auth.token', '');
+  if (!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
